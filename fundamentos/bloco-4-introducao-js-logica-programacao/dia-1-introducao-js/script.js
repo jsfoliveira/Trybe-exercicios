c9cf39dc2f0c481bcc9b4e85653f06e9521dd097
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-11-27 15:47:04 -0300

    Realizado todo o exercicio na parte do conteudo, falta fazer a lista de exercicios

---
+++ fundamentos/bloco-4-introducao-js-logica-programacao/dia-1-introducao-js/script.js
@@ -0,0 +1,95 @@
+//PARTE 1
+// const myName = "Juliana";
+
+// const birthCity = "Serrinha";
+
+// var birthYear = 1989;
+
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// birthYear = 2030;
+// console.log(birthYear);
+
+//PARTE 2
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: "Ana",
+//   lastName: "Santos",
+// };
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// const patientEmail = "ana@email.com";
+
+// const base = 5;
+// var height = 8;
+// const area = base * height;
+// console.log(area);
+
+// const lado = 10;
+// const perimeter = 3 * lado;
+// console.log(perimeter);
+
+//PARTE 3
+// const nota1 = 80;
+// if (nota1 >= 80) {
+//   console.log("Parabéns, você foi aprovada(o)!");
+// } else if (nota1 < 80 || nota1 == 60) {
+//   console.log("Você está na nossa lista de espera");
+// } else {
+//   console.log("Você foi reprovada(o)");
+// }
+
+//PARTE 4
+// const currentHour = 20;
+// var message;
+// if (currentHour >= 22) {
+//   console.log((message = "Não deveríamos comer nada, é hora de dormir"));
+// } else if (currentHour >= 18 && currentHour < 22) {
+//   console.log((message = "Rango da noite, vamos jantar :D"));
+// } else if (currentHour >= 14 && currentHour < 18) {
+//   console.log((message = "Hora do almoço!!!"));
+// } else if (currentHour < 14 && currentHour >= 11) {
+//   console.log((message = "Vamos fazer um bolo pro café da tarde?"));
+// } else if (currentHour < 11 && currentHour >= 4) {
+//   console.log((message = "Hmmm, cheiro de café recém passado"));
+// }
+// console.log(message);
+// var weekDay = "quarta-feira";
+// if (
+//   weekDay === "segunda-feira" ||
+//   weekDay === "terça-feira" ||
+//   weekDay === "quarta-feira" ||
+//   weekDay === "quinta-feira" ||
+//   weekDay === "sexta-feira"
+// ) {
+//   console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+// } else {
+//   console.log("FINALMENTE, descanso merecido UwU");
+// }
+
+// console.log(weekDay);
+
+//PARTE 5
+// var estadoPessoa = "aprovada";
+// switch (estadoPessoa) {
+//   case "aprovada":
+//     console.log("Aprovada");
+//     break;
+
+//   case "lista":
+//     console.log("Lista");
+//     break;
+
+//   case "reprovada":
+//     console.log("Reprovada");
+//     break;
+
+//   default:
+//     console.log("Não se aplica");
+// }
+
+//EXERCÍCIOS
+//QUESTÃO 01
