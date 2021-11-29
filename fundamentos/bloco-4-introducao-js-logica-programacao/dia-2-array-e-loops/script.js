e9da1e6bc0bcbbe402a8120ccdd5b3420cf233d7
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-11-29 16:02:49 -0300

    Finzalida a parte de atividade do conteudo, falta a lista de exercicio

---
+++ fundamentos/bloco-4-introducao-js-logica-programacao/dia-2-array-e-loops/script.js
@@ -0,0 +1,32 @@
+// Atividade 01: Obtenha o valor "Serviços" do array menu
+// let menu = ["Home", "Serviços", "Portfólio", "Links"];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// Atividade 02: Procure o índice do valor "Portfólio" do array menu
+// let menu = ["Home", "Serviços", "Portfólio", "Links"];
+// let indexOfPortfolio = menu[2];
+
+// console.log(indexOfPortfolio);
+
+// Atividade 03: Adicione o valor "Contato" no final do array menu
+// let menu = ["Home", "Serviços", "Portfólio", "Links"];
+// menu.push("Conteúdo");
+// console.log(menu);
+
+//Atividade 04: Utilize o for para imprimir os elementos da lista groceryList com o console.log()
+// let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+// for (let i = 0; i < groceryList.length; i++) {
+//   console.log(groceryList[i]);
+// }
+
+//Atividade 04:
+// let names = ["João", "Maria", "Antônio", "Margarida"];
+// for (let nome of names) {
+//   console.log(nome);
+// }
+let names = ["João", "Maria", "Antônio", "Margarida"];
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
