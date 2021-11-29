de5d5e1e10857aed77b12c4fbe042ae1b3e46363
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-11-29 20:50:40 -0300

    Feito ate o exercicio 01

--- fundamentos/bloco-4-introducao-js-logica-programacao/dia-2-array-e-loops/script.js
+++ fundamentos/bloco-4-introducao-js-logica-programacao/dia-2-array-e-loops/script.js
@@ -21,12 +21,34 @@
 //   console.log(groceryList[i]);
 // }
 
-//Atividade 04:
+//Atividade 05
 // let names = ["João", "Maria", "Antônio", "Margarida"];
 // for (let nome of names) {
 //   console.log(nome);
 // }
-let names = ["João", "Maria", "Antônio", "Margarida"];
-for (let i = 0; i < names.length; i++) {
-  console.log(names[i]);
-}
+
+//Atividade 06: mega sena, 6 nḿeros, array
+let megaSenaNumeros = [6, 8, 10, 12, 16, 18];
+
+//EXERCICIO 01: percorra o array imprimindo todos os valores nele contidos com a função console.log()
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+//EXERCICIO 02:some todos os valores contidos no array e imprima o resultado
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//EXERCICIO 03:
+
+//EXERCICIO 04:
+
+//EXERCICIO 05:
+
+//EXERCICIO 06:
+
+//EXERCICIO 07:
+
+//EXERCICIO 08:
+
+//EXERCICIO 09:
