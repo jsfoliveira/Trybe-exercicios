cc24a41c8f60c63d00ac1b955fb5ef7e41bcfa6c
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-11-29 20:52:08 -0300

    exercicio bloco 4 dia 1

--- fundamentos/bloco-4-introducao-js-logica-programacao/dia-1-introducao-js/script.js
+++ fundamentos/bloco-4-introducao-js-logica-programacao/dia-1-introducao-js/script.js
@@ -93,3 +93,101 @@
 
 //EXERCÍCIOS
 //QUESTÃO 01
+// const a = 10;
+// const b = 5;
+// console.log('Soma: ' + (a + b));
+// console.log('Subtração: ' + (a - b));
+// console.log('Multiplicação: ' + (a * b));
+// console.log('Divisão: ' + (a / b));
+// console.log('Módulo: ' + (a % b));
+
+// //QUESTÃO 02
+// const a = 20;
+// const b = 21;
+// if (a > b) {
+//   console.log("'a' é maior que 'b'");
+// } else {
+//   console.log("'b' é maior que 'a'");
+// }
+
+//QUESTÃO 03
+// const a = 6;
+// const b = 4;
+// const c = 2;
+
+// if (a > b && a > c) {
+//   console.log('O maior número é: ' + a + ' (a)');
+// } else if (b > a && b > c) {
+//   console.log('O maior número é: ' + b + ' (b)');
+// } else {
+//   console.log('O maior número é: ' + c + ' (c)');
+// };
+
+//QUESTÃO 04
+// const number = 4;
+
+// if (number > 0) {
+//   console.log("positive");
+// } else if (number < 0) {
+//   console.log("negative");
+// } else {
+//   console.log("zero");
+// }
+
+// // QUESTAO 05
+
+// let degreeAngleA = 65;
+// let degreeAngleB = 100;
+// let degreeAngleC = 15;
+
+// let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+// let allAnglesArePositives =
+//   degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+
+// if (allAnglesArePositives) {
+//   if (sumOfAngles === 180) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// } else {
+//   console.log("Erro: ângulo inválido");
+// }
+
+// //QUESTAO 06
+// const chessPiece = 'bispo';
+
+// switch (chessPiece.toLowerCase()) {
+//   case 'rei':
+//     console.log('Rei-> Uma casa apenas para qualquer direção.');
+//     break;
+//   case 'bispo':
+//     console.log('Bispo-> Diagonal.');
+//     break;
+//   case 'rainha':
+//     console.log('Rainha-> Diagonal, horizontal e vertical.');
+//     break;
+//   case 'cavalo':
+//     console.log('Cavalo -> "L" pode pular sobre as peças.');
+//     break;
+//   case 'torre':
+//     console.log('Torre -> Horizontal e vertical.');
+//     break;
+//   case 'peão':
+//     console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+//     break;
+//   default:
+//     console.log('Erro, peça inválida!');
+//     break;
+// };
+
+//QUESTAO 07
+
+//QESTAO 08
+
+//QUESTAO 09
+
+//QUESTAO 10
+
+//QUESTAO 11
