5d4449ff489227c570da2c1ab22c5eb00b734fa8
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-11-30 14:43:03 -0300

    realizei ate o exercicio 05

--- fundamentos/bloco-4-introducao-js-logica-programacao/dia-2-array-e-loops/script.js
+++ fundamentos/bloco-4-introducao-js-logica-programacao/dia-2-array-e-loops/script.js
@@ -27,9 +27,6 @@
 //   console.log(nome);
 // }
 
-//Atividade 06: mega sena, 6 nḿeros, array
-let megaSenaNumeros = [6, 8, 10, 12, 16, 18];
-
 //EXERCICIO 01: percorra o array imprimindo todos os valores nele contidos com a função console.log()
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // for (let i = 0; i < numbers.length; i++) {
@@ -37,18 +34,52 @@ let megaSenaNumeros = [6, 8, 10, 12, 16, 18];
 // }
 
 //EXERCICIO 02:some todos os valores contidos no array e imprima o resultado
+// let numeros = [5, 9, 3, 19, 70];
+// let soma = 0;
+// for (let i = 0; i < numeros.length; i++) {
+//   soma = soma + numeros[i];
+// }
+
+// console.log(soma);
+
+//EXERCICIO 03: Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// let numeros = [5, 9, 3, 19, 70];
+// let soma = 0;
+// for (let i = 0; i < numeros.length; i++) {
+//   soma = numeros[i] + numeros[i];
+// }
+
+// console.log(soma / numeros.length);
+
+//EXERCICIO 04: Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20".
+// let numeros = [5, 9, 3, 19, 70];
+// let soma = 0;
+// for (let i = 0; i < numeros.length; i++) {
+//   soma = (numeros[i] + numeros[i]) / numeros.length;
+// }
+// if (soma > 20) {
+//   console.log("Valor maior que 20");
+// } else {
+//   console.log("Valor menor ou igual a 20");
+// }
+
+//EXERCICIOS 05: Utilizando for , descubra qual o maior valor contido no array e imprima-o
+// let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maiorNumero = numeros[0];
+// for (let i = 1; i < numeros.length; i++) {
+//   if (numeros[0] > maiorNumero) {
+//     maiorNumero = numero[i];
+//   }
+// }
+// console.log(maiorNumero);
+
+//EXERCICIO 06
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = [0];
 
-//EXERCICIO 03:
-
-//EXERCICIO 04:
-
-//EXERCICIO 05:
-
-//EXERCICIO 06:
-
-//EXERCICIO 07:
-
-//EXERCICIO 08:
-
-//EXERCICIO 09:
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[0] % 2 !== 0) {
+    impar = impar + 1;
+  }
+}
