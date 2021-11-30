8345e774f840eea3e729eb2d80b15beb7e2ba6c7
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-11-30 15:23:53 -0300

    realizada a primeira atividade

---
+++ fundamentos/bloco-4-introducao-js-logica-programacao/dia-3-logica-programacao-algoritmo/script.js
@@ -0,0 +1,16 @@
+//definir array
+// let fruits = [3, 4, 10, 1, 12];
+//defina a variavel que guardará a soma
+// let soma = 0;
+//some todos os valores
+// for (let i = 0; i < fruits.length; i++) {
+//   soma = soma + fruits[0];
+// }
+//criar a condicional
+// if (soma > 15) {
+//   console.log("Valor maior");
+// } else if (soma <= 15) {
+//   console.log("Valor menor que 16");
+// }
+
+//EXERCICIO 01:Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
