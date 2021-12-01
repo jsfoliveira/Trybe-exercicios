320362a53e09c42a77c3880b6a21850861622340
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-11-30 22:16:37 -0300

    Realizei os exercicios de 1 a 3

--- fundamentos/bloco-4-introducao-js-logica-programacao/dia-3-logica-programacao-algoritmo/script.js
+++ fundamentos/bloco-4-introducao-js-logica-programacao/dia-3-logica-programacao-algoritmo/script.js
@@ -14,3 +14,26 @@
 // }
 
 //EXERCICIO 01:Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
+// let n = 5;
+// let str = "*";
+// for (i = 0; i < n; i++) {
+//   console.log(str.repeat(n));
+// }
+//EXERCICIO 02: Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+// let n = 5;
+// let m = n - 4;
+// let str = "*";
+// for (i = 0; i < n; i++) {
+//   console.log(str.repeat(m));
+//   m = m + 1;
+// }
+//EXERCICIO 03:Agora inverta o lado do triângulo
+// let n = 5;
+// let m = n - 4;
+// let str1 = "*";
+// let str2 = " ";
+// for (i = 0; i < n; i++) {
+//   console.log(str2.repeat(n - m) + str1.repeat(m));
+//   m = m + 1;
+// }
+//EXERCICIO 04:  Depois, faça uma pirâmide com n asteriscos de base
