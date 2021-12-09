f6f71ec9682bc29ef8a954919020ebf1a513cfd2
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-12-09 11:28:18 -0300

    fiz a primeira questao

--- fundamentos/bloco-5-js-dom-evenyos-webStore/bloco-5-dia-4/script.js
+++ fundamentos/bloco-5-js-dom-evenyos-webStore/bloco-5-dia-4/script.js
@@ -1,12 +1,15 @@
 //mudar o fundo do content
 let btnBackgroundColor = document.getElementById("background-color");
-function mudarCorFundo(event) {
-  let content = document.querySelector(".content");
-  event.target.content.style.backgroundColor = color;
-  localStorage.setItem("backgroundColor", color);
+let listaDeFilhos = btnBackgroundColor.children;
+console.log(listaDeFilhos);
+for (let i = 1; i < listaDeFilhos.length; i += 1) {
+  listaDeFilhos[i].addEventListener("click", function (event) {
+    let content = document.querySelector(".content");
+    let color = event.target.innerText;
+    content.style.backgroundColor = color;
+    localStorage.setItem("backgroundColor", color);
+  });
 }
 
-btnBackgroundColor.addEventListener("click", oi);
-
-window.onload = function () {};
+// window.onload = function () {};
 //event.target vai sinalizar qual o elemento precisa ser chamado. ele vai sinalizar o background-color. no background-color vai dizer qual a cor que vai ficar no (color)
