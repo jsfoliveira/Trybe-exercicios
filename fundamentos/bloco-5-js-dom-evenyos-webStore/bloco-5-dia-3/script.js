9b3d06fd49d8d063e6eedb646b1d5b6fb6a19996
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-12-27 17:21:20 -0300

    finalizado exercicio spotrybefy

--- fundamentos/bloco-5-js-dom-evenyos-webStore/bloco-5-dia-3/script.js
+++ fundamentos/bloco-5-js-dom-evenyos-webStore/bloco-5-dia-3/script.js
@@ -5,23 +5,30 @@
 // NÃO ENTENDI 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
-const segundoLi = document.getElementsById("second-li");
-function mudarLi(event) {
-  const techElement = document.querySelector(".tech");
+let container = document.querySelector('.container');
+let filhosContainer = container.children;
+for (let i = 0; i < filhosContainer.length; i += 1){
+  function adicionarTech() {
+    if (filhosContainer[i].className == 'tech'){
+      filhosContainer[i].classList.remove ('tech');
+      // filhosContainer[i].classList.add('tech');
+    } else {
+      filhosContainer[i].classList.add('tech');
+    }
 }
-adicionarTech.addEventListener("", function () {});
+filhosContainer[i].addEventListener('click', adicionarTech);
+}
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
-input.addEventListener("input", function (event) {
-  const techElement = document.querySelector(".tech");
-  techElement.innerText = event.target.value;
-});
+
+function alterarTexto (event){
+  const firstElement = document.querySelector("#first-li");
+  firstElement.innerText = event.target.value;
+
+};
+input.addEventListener("input", alterarTexto);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
-//cria a variavel de onde o elemento que vai ser acionado está
-//cria a função: function dáumnome (){
-// o evento que quer que faça
-//}
-//cria o addEventListener: variavel.addEventListener ("evento", puxa a função pra cá)
+
 let top3Spotrybefy = document.getElementById("my-spotrybefy");
 
 top3Spotrybefy.addEventListener("dblclick", function click2Vezes() {
@@ -30,21 +37,22 @@ top3Spotrybefy.addEventListener("dblclick", function click2Vezes() {
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
 
-top3Spotrybefy.addEventListener("mouseover", function passarMouseEmCima(event) {
+let titulo = document.querySelector('h1');
+titulo.addEventListener("mouseover", function passarMouseEmCima(event) {
   event.target.style.backgroundColor = "red";
 });
+
+
+
 // Segue abaixo um exemplo do uso de event.target:
 
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = "Opção reiniciada";
-  event.target.style.backgroundColor = "black";
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
+// function resetText(event) {
+//   // O Event é passado como um parâmetro para a função.
+//   event.target.innerText = "Opção reiniciada";
+//   event.target.style.backgroundColor = "black";
+//   // O event possui várias propriedades, porém a mais usada é o event.target,
+//   // que retorna o objeto que disparou o evento.
+// }
 
-firstLi.addEventListener("dblclick", resetText);
+// firstLi.addEventListener("dblclick", resetText);
 
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
