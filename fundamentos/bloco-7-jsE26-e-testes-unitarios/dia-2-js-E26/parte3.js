233e5f74b05f4a886e01264dc3b156d144a9e7c5
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-01-14 11:08:22 -0300

    exercicio do bloco 6, dia 2 finalizado

--- fundamentos/bloco-7-jsE26-e-testes-unitarios/dia-2-js-E26/parte3.js
+++ fundamentos/bloco-7-jsE26-e-testes-unitarios/dia-2-js-E26/parte3.js
@@ -47,13 +47,31 @@ console.log(valoresObjeto(lesson2));
 
 //5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
 const allLessons = {};
-Object.assign(allLessons, lesson1, lesson2, lesson3);
+Object.assign(allLessons, {lesson1, lesson2, lesson3});
 console.log(allLessons);
 
-
-
 //6. Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+// function somEstudantes (){
+  const totalEstudante = lesson1.numeroEstudantes + lesson2.numeroEstudantes + lesson3.numeroEstudantes;
+  
+// }
+console.log(totalEstudante);
 
-//7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+//7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+function valorPosicao (objeto, indice){
+  return Object.values(lesson1)[indice];
+}
+console.log(valorPosicao(lesson1,0));
 
-//8. Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
+//8. Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
+function parExiste (objeto, chave, valor){
+  const valor1 =  Object.entries(objeto);
+  console.log(valor1);
+  for(let i =0; i < valor1.length; i += 1){
+    if (valor1[0][0] === chave && valor1[0][1] === valor) { return true;
+    } else {
+    return false;
+  }
+  }
+};
+console.log(parExiste(lesson1,'materia', 'Matemática'));
