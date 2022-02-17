9b8a6c7ef7c40d841c1f13c45d71e031dba779a7
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-02-16 23:02:44 -0300

    iniciando exercicio de react

---
+++ front-end/bloco-10-introducao-react/dia-1-introducao/testando-meu-computador/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Hello Word</h1>
+                <p>Bem-vindo ao mundo do ReactJS.</p>
+            </>
+        )
+    }
+}
+
+export default Header;
