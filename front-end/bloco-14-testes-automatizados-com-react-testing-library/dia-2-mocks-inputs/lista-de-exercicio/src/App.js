b43a05f4b2d044909f227ae08dcc7a82e5cc6239
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-03-21 14:47:56 -0300

    iniciando o exercicio

---
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-mocks-inputs/lista-de-exercicio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      {Math.floor(Math.random() * 10)}
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-mocks-inputs/lista-de-exercicio/src/Service.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Service from './Service';
+
+describe ('Estudando teste', () =>  {
+  it('oioioi', () => {
+render(<Service />)
+
+  });
+});
